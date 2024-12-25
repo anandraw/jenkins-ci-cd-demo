cebd791eedc05de65fcec626f8a23eec0d393614
Anand Rawool <anandrawool5624@gmail.com>
2024-12-25 17:49:14 +0530

    changes done

--- src/test/java/com/anand/CiCddemoApplicationTests.java
+++ src/test/java/com/anand/CiCddemoApplicationTests.java
@@ -3,13 +3,11 @@ package com.anand;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
-@WebMvcTest(controllers = CiCddemoApplication.class)
+@WebMvcTest
 class CiCddemoApplicationTests {
 	
 
